1380b2203e4887b3b66caca6bdda5e8ba267840e
Doug L <leedke@gmail.com>
2020-06-08 16:11:10 -0500

    Mobile overlay and sticky navbar added

---
+++ blog/static/blog/blog.js
@@ -0,0 +1,20 @@
+
+var overlay = false;
+
+function toggleNav(){
+    if(overlay) {
+        document.getElementById("myNav").style.width = "0%";
+        overlay = false;
+    } else {
+        document.getElementById("myNav").style.width = "100%";
+        overlay = true;
+    }
+}
+
+function openNav() {
+   
+}
+  
+  function closeNav() {
+    
+}
