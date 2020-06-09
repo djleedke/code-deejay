e56f1097083370d4910aaeeaf7f72c85f9aadd2b
Doug L <leedke@gmail.com>
2020-06-08 21:04:26 -0500

    CSS overhaul, added footer

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -3,18 +3,10 @@ var overlay = false;
 
 function toggleNav(){
     if(overlay) {
-        document.getElementById("myNav").style.width = "0%";
+        document.getElementById("overlay").style.width = "0%";
         overlay = false;
     } else {
-        document.getElementById("myNav").style.width = "100%";
+        document.getElementById("overlay").style.width = "100%";
         overlay = true;
     }
-}
-
-function openNav() {
-   
-}
-  
-  function closeNav() {
-    
 }
